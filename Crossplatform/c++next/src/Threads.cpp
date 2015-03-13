1b98f09c8cb6fef7eb8ac416df845adc554b867b
Sergey Nikonov <dyver@hotmail.com>
2015-03-13 12:51:58 +0300

    c++next: улучшен тест многопоточности

--- Crossplatform/c++next/src/Threads.cpp
+++ Crossplatform/c++next/src/Threads.cpp
@@ -5,17 +5,39 @@
 
 // Threads
 
+using SD = std::shared_ptr<int>;
+
+static const int Times = 1000000;
+
+// standalone task function
+void taskFunction(SD data, std::mutex& mutex) {
+    for (auto i = 0; i < Times; ++i) {
+        mutex.lock();
+        ++*data;
+        mutex.unlock();
+    }
+}
+
 void Threads() {
     try {
         std::mutex mutex;
-        const int times = 1000000;
         // data shared between threads
-        typedef std::shared_ptr<int> SD;
+        // note, that we can pass this wrapped data by value or reference due to shared_ptr
         SD data(new int());
-        *data = 0;
-        // lambda function
+        // we will have 5 threads doing equal opposite work, so initial value must be -Times
+        // to yeld in a 0
+        *data = -Times;
+        // lambda function with closured data
         auto lf = [data, &mutex]() {
-            for (auto i = 0; i < times; ++i) {
+            for (auto i = 0; i < Times; ++i) {
+                mutex.lock();
+                --*data;
+                mutex.unlock();
+            }
+        };
+        // lambda function with data passed throw argument
+        auto lfa = [](SD data, std::mutex& mutex) {
+            for (auto i = 0; i < Times; ++i) {
                 mutex.lock();
                 ++*data;
                 mutex.unlock();
@@ -26,34 +48,34 @@ void Threads() {
             SD& data;
             std::mutex &mutex;
             Sf(SD& d, std::mutex& m) : data(d), mutex(m) {}
-            void operator ()() {
-                for (auto i = 0; i < times; ++i) {
+            void operator()() {
+                for (auto i = 0; i < Times; ++i) {
                     mutex.lock();
                     --*data;
                     mutex.unlock();
                 }
             }
         };
-        // lambda function with argument
-        auto lfa = [](SD data, std::mutex& mutex) {
-            for (auto i = 0; i < times; ++i) {
-                mutex.lock();
-                --*data;
-                mutex.unlock();
-            }
-        };
+        outHeader("Threads & mutexes");
+        outIdent();
+        std::cout << "initial value of shared data = " << *data << std::endl;
+        outIdent();
+        std::cout << "doing work in 5 threads ..." << std::flush;
         // making and starting threads
-        std::thread th1{lf};
-        std::thread th2{Sf(data, mutex)};
-        //std::thread th3{std::bind(lfa, data, mutex)}; // can't bind mutex
+        std::thread th1{taskFunction, data, std::ref(mutex)};
+        std::thread th2{lf};
+        std::thread th3{Sf(data, mutex)};
+        std::thread th4{std::bind(lfa, data, std::ref(mutex))};
         // do some work in main thread
         lfa(data, mutex);
         // waiting for threads ending
         th1.join();
         th2.join();
-        outHeader("Threads & mutexes");
+        th3.join();
+        th4.join();
+        std::cout << " done." << std::endl;
         outIdent();
-        std::cout << "shared data = " << *data << std::endl;
+        std::cout << "processed shared data = " << *data << " (must be = 0)" << std::endl;
     } catch (const std::exception& e) {
         std::cout << "Exception" << e.what() << std::endl;
     }
